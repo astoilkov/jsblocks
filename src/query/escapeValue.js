301e71075963f2e45c607e591528184a26755d8d
Antonio Stoilkov <antonio.stoilkov@gmail.com>
2015-05-03 16:19:27 +0300

    do not escape unnecessary symbols

--- src/query/escapeValue.js
+++ src/query/escapeValue.js
@@ -1,21 +1,11 @@
 define(function () {
   var ampRegEx = /&/g;
-  var quotRegEx = /"/g;
-  var singleQuoteRegEx = /'/g;
   var lessThanRegEx = /</g;
-  var greaterThanRegEx = />/g;
+
   function escapeValue(value) {
     return String(value)
       .replace(ampRegEx, '&amp;')
-      .replace(quotRegEx, '&quot;')
-      .replace(singleQuoteRegEx, '&#39;')
-      .replace(lessThanRegEx, '&lt;')
-      .replace(greaterThanRegEx, '&gt;');
-    // return document
-    //   .createElement('a')
-    //   .appendChild(document.createTextNode(value))
-    //     .parentNode
-    //     .innerHTML;
+      .replace(lessThanRegEx, '&lt;');
   }
 
   return escapeValue;
