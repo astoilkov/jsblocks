70c702a48969a62177ff7c3548c42d60cf3624e9
Antonio Stoilkov <antonio.stoilkov@gmail.com>
2015-07-21 09:33:49 +0300

    resolve #72

--- src/mvc/clonePrototype.js
+++ src/mvc/clonePrototype.js
@@ -6,40 +6,26 @@ define([
     var key;
     var value;
 
-    if (prototype.__used__) {
-      for (key in prototype) {
-        value = prototype[key];
-        if (Property.Is(value)) {
-          continue;
-        }
-
-        if (blocks.isObservable(value)) {
-          // clone the observable and also its value by passing true to the clone method
-          object[key] = value.clone(true);
-          object[key].__context__ = object;
-        } else if (blocks.isFunction(value)) {
-          object[key] = blocks.bind(value, object);
-        } else if (Model.prototype.isPrototypeOf(value)) {
-          object[key] = value.clone(true);
-        } else if (blocks.isObject(value) && !blocks.isPlainObject(value)) {
-          object[key] = blocks.clone(value, true);
-        } else {
-          object[key] = blocks.clone(value, true);
-        }
+    for (key in prototype) {
+      value = prototype[key];
+      if (Property.Is(value)) {
+        continue;
       }
-    } else {
-      for (key in prototype) {
-        value = prototype[key];
-        if (blocks.isObservable(value)) {
-          value.__context__ = object;
-        } else if (blocks.isFunction(value)) {
-          object[key] = blocks.bind(value, object);
-          object[key].unbound = value;
-        }
+
+      if (blocks.isObservable(value)) {
+        // clone the observable and also its value by passing true to the clone method
+        object[key] = value.clone(true);
+        object[key].__context__ = object;
+      } else if (blocks.isFunction(value)) {
+        object[key] = blocks.bind(value, object);
+      } else if (Model.prototype.isPrototypeOf(value)) {
+        object[key] = value.clone(true);
+      } else if (blocks.isObject(value) && !blocks.isPlainObject(value)) {
+        object[key] = blocks.clone(value, true);
+      } else {
+        object[key] = blocks.clone(value, true);
       }
     }
-
-    prototype.__used__ = true;
   }
 
   return clonePrototype;
