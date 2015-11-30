cdc74eaa6831367f71d856aa0d61e15d21bdfd11
Joscha Rohmann <joscha-rohmann@online.de>
2015-11-30 23:05:20 +0100

    Added blocks.middleware.
    As discussed in #95.

--- src/node/methods.js
+++ src/node/methods.js
@@ -1,9 +1,32 @@
 define([
   '../core',
   './Server',
-], function (blocks, Server) {
+  './Middleware'
+], function (blocks, Server, Middleware) {
   blocks.server = function (options) {
     return new Server(options);
+  };
+
+  blocks.middleware = function (options) {
+  	var express = require('express');
+  	var path = require('path');
+  	// the real middleware
+  	var middleware;
+  	// array of middlewares to return
+  	var middlewares = [];
+
+  	options = blocks.extend({}, Middleware.Defaults, options);
+  	middleware = new Middleware(options);
+
+  	// express.static is required as the frontend app needs it's files
+  	middlewares.push(express.static(path.resolve(options.static), {
+  		index: false
+  	}));
+
+  	middlewares.push(blocks.bind(middleware.tryServePage, middleware));
+
+  	// returning array of express middlewares that express will call in that order
+  	return middlewares;
   };
 
   blocks.static = function (options) {
