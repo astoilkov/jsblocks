2f2c57fdb13a1fffef9cc8088dc5b4f8d48763b6
Joscha Rohmann <rohmann@boros.de>
2016-10-17 12:17:10 +0200

    query/ExtenderHelper: Changed if-else-constructs to switch statements.

--- src/query/ExtenderHelper.js
+++ src/query/ExtenderHelper.js
@@ -3,6 +3,14 @@ define([
   '../modules/Events',
   './Observer'
 ], function (blocks, Events, Observer) {
+  
+  var Action = {
+    NOOP: 0,
+    ADD: 1,
+    REMOVE: 2,
+    EXISTS: 3
+  };
+
   var ExtenderHelper = {
     waiting: {},
     operations: {
@@ -106,10 +114,7 @@ define([
     },
 
     executeOperationsChunk: function (observable, operations) {
-      var ADD = 'add';
-      var REMOVE = 'remove';
-      var EXISTS = 'exists';
-      var action = EXISTS;
+      var action = Action.EXISTS;
 
       var collection = observable.__value__;
       var view = observable.view;
@@ -121,40 +126,44 @@ define([
       var take = collection.length;
       view.update = blocks.noop;
 
-      blocks.each(operations, function (operation) {
-        if (operation.type == ExtenderHelper.operations.SKIP) {
-          skip = operation.skip;
-          if (blocks.isFunction(skip)) {
-            skip = skip.call(observable.__context__);
-          }
-          skip = blocks.unwrap(skip);
-        } else if (operation.type == ExtenderHelper.operations.TAKE) {
-          take = operation.take;
-          if (blocks.isFunction(take)) {
-            take = take.call(observable.__context__);
-          }
-          take = blocks.unwrap(take);
-        } else if (operation.type == ExtenderHelper.operations.SORT) {
-          if (blocks.isString(operation.sort)) {
-            collection = blocks.clone(collection).sort(function (valueA, valueB) {
-              valueA = blocks.unwrap(valueA[operation.sort]);
-              valueB = blocks.unwrap(valueB[operation.sort]);
-              if (valueA > valueB) {
-                return 1;
-              }
-              if (valueA < valueB) {
-                return -1;
-              }
-              return 0;
-            });
-          } else if (blocks.isFunction(operation.sort)) {
-            collection = blocks.clone(collection).sort(operation.sort.bind(observable.__context__));
-          } else {
-            collection = blocks.clone(collection).sort();
-          }
-          if (operations.length == 1) {
-            operations.push({ type: ExtenderHelper.operations.FILTER, filter: function () { return true; }});
-          }
+      blocks.each(operations, function prepareOperations(operation) {
+        switch (operation.type) {
+          case ExtenderHelper.operations.SKIP:
+            skip = operation.skip;
+            if (blocks.isFunction(skip)) {
+              skip = skip.call(observable.__context__);
+            }
+            skip = blocks.unwrap(skip);
+            break;
+          case ExtenderHelper.operations.TAKE:
+            take = operation.take;
+            if (blocks.isFunction(take)) {
+              take = take.call(observable.__context__);
+            }
+            take = blocks.unwrap(take);
+            break;
+          case ExtenderHelper.operations.SORT:
+            if (blocks.isString(operation.sort)) {
+              collection = blocks.clone(collection).sort(function (valueA, valueB) {
+                valueA = blocks.unwrap(valueA[operation.sort]);
+                valueB = blocks.unwrap(valueB[operation.sort]);
+                if (valueA > valueB) {
+                  return 1;
+                }
+                if (valueA < valueB) {
+                  return -1;
+                }
+                return 0;
+              });
+            } else if (blocks.isFunction(operation.sort)) {
+              collection = blocks.clone(collection).sort(operation.sort.bind(observable.__context__));
+            } else {
+              collection = blocks.clone(collection).sort();
+            }
+            if (operations.length == 1) {
+              operations.push({ type: ExtenderHelper.operations.FILTER, filter: function () { return true; }});
+            }
+            break;
         }
       });
 
@@ -165,59 +174,67 @@ define([
           }
           return false;
         }
+
         blocks.each(operations, function executeExtender(operation) {
           var filterCallback = operation.filter;
+          operation.type = operation.type || (filterCallback && ExtenderHelper.operations.FILTER);
 
-          action = undefined;
+          action = Action.NOOP;
 
-          if (filterCallback) {
-            if (filterCallback.call(observable.__context__, value, index, collection)) {
-              action = EXISTS;
+          switch (operation.type) {
+            case ExtenderHelper.operations.FILTER:
+              if (filterCallback.call(observable.__context__, value, index, collection)) {
+                action = Action.EXISTS;
 
-              if (connections[index] === undefined) {
-                action = ADD;
+                if (connections[index] === undefined) {
+                  action = Action.ADD;
+                }
+              } else {
+                action = Action.NOOP;
+                if (connections[index] !== undefined) {
+                  action = Action.REMOVE;
+                }
+                return false;
               }
-            } else {
-              action = undefined;
-              if (connections[index] !== undefined) {
-                action = REMOVE;
-              }
-              return false;
-            }
-          } else if (operation.type == ExtenderHelper.operations.SKIP) {
-            action = EXISTS;
-            skip -= 1;
-            if (skip >= 0) {
-              action = REMOVE;
-              return false;
-            } else if (skip < 0 && connections[index] === undefined) {
-              action = ADD;
-            }
-          } else if (operation.type == ExtenderHelper.operations.TAKE) {
-            if (take <= 0) {
-              action = REMOVE;
-              return false;
-            } else {
-              take -= 1;
-              action = EXISTS;
+              break;
 
-              if (connections[index] === undefined) {
-                action = ADD;
+            case ExtenderHelper.operations.SKIP:
+              action = Action.EXISTS;
+              skip -= 1;
+              if (skip >= 0) {
+                action = Action.REMOVE;
+                return false;
+              } else if (skip < 0 && connections[index] === undefined) {
+                action = Action.ADD;
               }
-            }
+              break;
+
+            case ExtenderHelper.operations.TAKE:
+              if (take <= 0) {
+                action = Action.REMOVE;
+                return false;
+              } else {
+                take -= 1;
+                action = Action.EXISTS;
+
+                if (connections[index] === undefined) {
+                  action = Action.ADD;
+                }
+              }
+              break;
           }
         });
 
         switch (action) {
-          case ADD:
+          case Action.ADD:
             newConnections[index] = viewIndex;
             view.splice(viewIndex, 0, value);
             viewIndex++;
             break;
-          case REMOVE:
+          case Action.REMOVE:
             view.removeAt(viewIndex);
             break;
-          case EXISTS:
+          case Action.EXISTS:
             newConnections[index] = viewIndex;
             if (view.__value__.indexOf(collection[index]) != index) {
               view.move(view.__value__.indexOf(collection[index]), index);
