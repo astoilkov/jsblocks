bf6c5a771306e91f244e1ececd6753c43da2c647
Joscha Rohmann <joscha@kanaye.net>
2017-04-04 23:06:02 +0200

    debug logging for karma tests.

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,3 +1,5 @@
+console.log(process.env);
+
 module.exports = function (grunt) {
   var karmaConfig = {
     options: {
@@ -144,7 +146,7 @@ module.exports = function (grunt) {
       singleRun: true
     }
   };
-
+  console.log(JSON.stringify(karmaConfig, null ,2));
   grunt.config.set('karma', karmaConfig);
 
   grunt.registerTask('test', function (browser) {
